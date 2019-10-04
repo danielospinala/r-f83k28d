fa9c5c0d3585da1949dd197dcc3753b8f3f7e8c1
danielospina@gmail.com <danielospina@gmail.com>
2019-10-03 19:33:05 -0500

    incrementa 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    count: 0
+  };
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
   render() {
     return (
+
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.handleClick}>Incrementa</button>
       </div>
+
     );
   }
 }
